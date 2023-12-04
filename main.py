d6fe4d76ca037c9141b8518a3c59936e842d7bf5
R K MAHARANA <rkm.7387@gmail.com>
2023-12-04 06:17:11 +0530

    Learning: Match Case

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-user_prompt = "Enter a todo: "
-
 # Creating an Empty List:
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
+    user_action = input("Type add, show or exit:")
+
+    # Using Match Case: it is available after python 3.10 onwards.
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+
+
+
+
