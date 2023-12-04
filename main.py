a9d9366caeb3cd4eccdbc077e8a4e56fc1e3148f
R K MAHARANA <rkm.7387@gmail.com>
2023-12-04 18:02:26 +0530

    Adding: Complete action.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit:").strip()
+    user_action = input("Type add, show, edit, complete or exit:").strip()
 
     # Using Match Case: it is available after python 3.10 onwards.
     match user_action:
@@ -11,12 +11,15 @@ while True:
             todos.append(todo)
         case 'show' | 'display':
             for index, item in enumerate(todos):
-                row = f"{index}-{item}"
+                row = f"{index+1}-{item}"
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))-1
             new_todo = input("Enter the new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to edit: ")) - 1
+            todos.pop(number)
         case 'exit':
             break
         case _:
