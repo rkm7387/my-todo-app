0cf4e318536626512bea3150c8c468cefeed6cc6
R K MAHARANA <rkm.7387@gmail.com>
2023-12-04 16:49:09 +0530

    Adding Edit option.

--- main.py
+++ main.py
@@ -2,17 +2,24 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit:")
+    user_action = input("Type add, show, edit or exit:").strip()
 
     # Using Match Case: it is available after python 3.10 onwards.
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ").title()
             todos.append(todo)
-        case 'show':
-            print(todos)
+        case 'show' | 'display':
+            for item in todos:
+                print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))-1
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
+        case _:
+            print("You entered an unknown command.")
 
 
 
