7fafbfd398667c5a7d5138dd9b9095984fdcbf9c
R K MAHARANA <rkm.7387@gmail.com>
2023-12-04 06:01:54 +0530

    Learning While Loop

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+user_prompt = "Enter a todo: "
 
-todos = [todo1, todo2, todo3]
+# Creating an Empty List:
+todos = []
 
-print(todos)
-
-
-
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
