a143f9a5b9c075662546678e98639b26141db455
R K MAHARANA <rkm.7387@gmail.com>
2023-12-06 05:10:37 +0530

    Learning: One method for calling functions from different file.

---
+++ functions.py
@@ -0,0 +1,11 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write to do item in the text file."""
+    with open("todos.txt", 'w') as file_local:
+        file_local.writelines(todos_arg)
